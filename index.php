92f7573d8cc96f2717ed37717d860b2378e3bc26
Chris Adams <wave@chrisadams.me.uk>
2013-04-29 10:40:18 +0100

    Add some more explanation

--- index.php
+++ index.php
@@ -21,15 +21,63 @@ $twiml_url = getenv('TWIML_URL');
 // create our client
 $client = new Services_Twilio($sid, $token);
 
-// and make our call...
-$call = $client->account->calls->create( $call_from, $call_to, $twiml_url);
+
+if (isset($_GET['troll'])) {
+
+  if ($_GET['troll'] == TRUE) {
+    // $call = $client->account->calls->create( $call_from, $call_to, $twiml_url);
+    };
+};
+
 
 ?>
+<link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/assets/css/bootstrap.css">
+
 </head>
 <!-- And now let's display something on the actual page, too -->
 <body>
 
-<h1>You can't see jack, but this number is being called!</h1>
-<h1>Calling from <?php print($calling_from); ?> to <?php print($calling_to); ?></h1>
+<?php
+if (isset($_GET['troll'])) {
+  if ($_GET['troll'] == TRUE) {
+
+?>
+
+<h1>HUZZAH SING ALONG WITH THE TROLL SONG!</h1>
+
+<iframe width="640" height="315" src="http://www.youtube.com/embed/2Z4m4lnjxkY" frameborder="0" allowfullscreen></iframe>
+
+<h1>You might want to call them to make sure they saw the funny side, and apologise if they didn't.</h1>
+
+<h1>Here's their number: <?php echo $calling_to; ?></h1>
+
+<?php
+  };
+} else {
+
+?>
+
+
+
+<h1>You're about to make a call from: </h1>
+
+  <h1><?php print($calling_from); ?></h1>
+
+  <h1>to</h1>
+
+  <h1><?php print($calling_to); ?></h1>
+
+<hr />
+
+<h1>Will they see the funny side?</h1>
+
+<hr />
+
+<a href="/?troll=1" class="btn btn-large btn-success">Yes of course</a>
+
+<a href="http://google.com" class="btn btn-large btn-danger">No, and they can run faster than me.</a>
+
+<?php }; ?>
+
 </body>
 </html>
